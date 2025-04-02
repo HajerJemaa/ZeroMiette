2cfecc1e6aa8190f74bbf265c12fea68384a508f
Amoura29 <amira.bouafif7434@istic.ucar.tn>
2025-04-02 22:06:01 +0100

    modifying getall of table users making it one file

--- projweb/backend/users/getAllUserbyState.php
+++ projweb/backend/users/getAllUserbyState.php
@@ -2,8 +2,10 @@
 header("content-Type:application/json");
 $resultat=["message"=>"","data"=>null];
 require_once("../connexion.php");
-$reqsql="select * from users where state='accepted'";
+$reqsql="select * from users where state=:s";
 $rp=$connexion->prepare($reqsql);
+$x=$_GET['state'];
+$rp->bindParam(":s",$x);
 $r=$rp->execute();
 $r=$rp->fetchAll(PDO::FETCH_ASSOC);
 if ($r){
