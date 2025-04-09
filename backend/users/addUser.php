ff92eccb262b07f756d3b33398959a26ab0e5f4f
Amoura29 <amira.bouafif7434@istic.ucar.tn>
2025-04-09 22:51:30 +0100

    modifying add user since the data is not json

--- backend/users/addUser.php
+++ backend/users/addUser.php
@@ -2,9 +2,18 @@
 header("content-type:application/json");
 require_once("../connexion.php");
 $resultat=["message"=>"","data"=>null];
-//récupère le body
-$body =file_get_contents("php://input");
-$donnees= json_decode($body, true);
+//get the data 
+$last_Name = $_POST['ln'];
+$first_Name = $_POST['fn'];
+$user_name = $_POST['un'];
+$email = $_POST['mai'];
+$pwd = $_POST['pwd'];
+$region = $_POST['region'];
+$address = $_POST['add'];
+$number = $_POST['num'];
+$role = $_POST['rad'];
+$proof = file_get_contents($_FILES['proof']['tmp_name']);
+$description = $_POST['desc'];
 $reqsql="insert into users (last_name,first_name,user_name,email,pwd,region,address,number,role,proof,description) values (:ln,:fn,:un,:em,:pwd,:reg,:add,:num,:r,:p,:d)";
 $rp =$connexion->prepare($reqsql); 
 $rp->bindParam(":ln",$donnees['last_name']);
