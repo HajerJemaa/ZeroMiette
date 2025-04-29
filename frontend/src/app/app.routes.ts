acdfa5ec07ebd8fc71ef460f1f84a3538b5aa607
dalynour <delinour73@gmail.com>
2025-04-30 00:06:41 +0100

    sss

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,13 +6,13 @@ import { GetAllUsersComponent } from './get-all-users/get-all-users.component';
 import { AddUserComponent } from './add-user/add-user.component';
 import { GetUserRequestsByStateComponent } from './get-user-requests-by-state/get-user-requests-by-state.component';
 import {GetAnnByStateComponent}from './get-ann-by-state/get-ann-by-state.component'
+import {DashbordDemandeurComponent} from './dashbord-demandeur/dashbord-demandeur.component'
 //import { GetDonorAnnouncementsComponent } from './get-donor-announcements/get-donor-announcements.component';
 import { AuthenticateComponent } from './authenticate/authenticate.component';
 import { RecieverComponent } from './reciever/reciever.component';
 import { DonorComponent } from './donor/donor.component';
 import { GetAnnComponent } from './get-ann/get-ann.component';
 import { administratorGuard } from './administrator.guard';
-
 
 export const routes: Routes = [
     {path:'', component:AccueilComponent},    
