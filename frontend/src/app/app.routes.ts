19bffe7636af9463d0d7ba0c9e5a4d9696134551
Amoura29 <amira.bouafif7434@istic.ucar.tn>
2025-05-01 12:56:57 +0100

    ii

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-import { Component } from '@angular/core';
 import { AccueilComponent } from './accueil/accueil.component';
 import { GetUserComponent } from './get-user/get-user.component';
 import { GetAllUsersComponent } from './get-all-users/get-all-users.component';
@@ -8,7 +7,6 @@ import { GetUserRequestsByStateComponent } from './get-user-requests-by-state/ge
 import {GetAnnByStateComponent}from './get-ann-by-state/get-ann-by-state.component'
 import {DashbordDemandeurComponent} from './dashbord-demandeur/dashbord-demandeur.component'
 import { AuthenticateComponent } from './authenticate/authenticate.component';
-import { DonorComponent } from './donor/donor.component';
 import { GetAnnComponent } from './get-ann/get-ann.component';
 import { administratorGuard } from './administrator.guard';
 
