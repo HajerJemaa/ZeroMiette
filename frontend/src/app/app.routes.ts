8755b330ff7fe1caf4887df973f83a56d9add869
Amoura29 <amira.bouafif7434@istic.ucar.tn>
2025-05-01 12:40:03 +0100

    kkk

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -7,7 +7,7 @@ import { AddUserComponent } from './add-user/add-user.component';
 import { GetUserRequestsByStateComponent } from './get-user-requests-by-state/get-user-requests-by-state.component';
 import {GetAnnByStateComponent}from './get-ann-by-state/get-ann-by-state.component'
 import {DashbordDemandeurComponent} from './dashbord-demandeur/dashbord-demandeur.component'
-//import { GetDonorAnnouncementsComponent } from './get-donor-announcements/get-donor-announcements.component';
+import { GetDonorAnnouncementsComponent } from './get-donor-announcements/get-donor-announcements.component';
 import { AuthenticateComponent } from './authenticate/authenticate.component';
 import { RecieverComponent } from './reciever/reciever.component';
 import { DonorComponent } from './donor/donor.component';
@@ -19,12 +19,12 @@ export const routes: Routes = [
     {path:'Administrator/ProcessAccount/getAllUsers/:state', component:GetAllUsersComponent,},
     {path:'Administrator/ProcessAccount/getUser/:id', component:GetUserComponent},
     {path:'User/CreateAccount/SignUp',component:AddUserComponent},
-   // {path:'User/Authenticate/:action', component:AuthenticateComponent},
+    {path:'User/Authenticate/:action', component:AuthenticateComponent},
     {path:'Reciever', component:DashbordDemandeurComponent},
     {path:'Donor', component:DonorComponent},
     {path:'get-user-requests-by-state', component: GetUserRequestsByStateComponent },
     {path:'get-ann-by-state', component:GetAnnByStateComponent},
     {path:'Administrator/ConsultAnnouncement', component:GetAnnComponent}
-    //{path: 'get-donor-announcements' , component:GetDonorAnnouncementsComponent}
+    {path: 'get-donor-announcements' , component:GetDonorAnnouncementsComponent}
 
 ];
