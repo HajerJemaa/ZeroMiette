a124b7650898efd44ec14409d8bfadc8245ebad0
Amoura29 <amira.bouafif7434@istic.ucar.tn>
2025-05-08 18:42:24 +0100

    i completed update user and update admin but not the backend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -13,6 +13,7 @@ import { administratorGuard } from './administrator.guard';
 import { recieverGuard } from './reciever.guard';
 import { UpdateUserComponent } from './update-user/update-user.component';
 import { AdministratorComponent } from './administrator/administrator.component';
+import { UpdateAdminComponent } from './update-admin/update-admin.component';
 
 
 export const routes: Routes = [
@@ -21,6 +22,7 @@ export const routes: Routes = [
     {path:'Administrator/ProcessAccount/getAllUsers/:state', component:GetAllUsersComponent, canActivate:[administratorGuard]},
     {path:'Administrator/ProcessAccount/getUser/:id', component:GetUserComponent, canActivate:[administratorGuard]},
     {path:'Administrator/ConsultAnnouncement', component:GetAnnComponent, canActivate:[administratorGuard]},
+    {path:'Administrator/Profile', component:UpdateAdminComponent, canActivate:[administratorGuard]},
 
     {path:'User/CreateAccount/SignUp',component:AddUserComponent},
     {path:'User/Authenticate/:action', component:AuthenticateComponent},
